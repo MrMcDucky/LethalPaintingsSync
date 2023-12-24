07dff82db8d66de5aff04a6002e40a19d0993100
Hugo Sandell <hugossandell@gmail.com>
2023-12-24 21:21:14 +0100

    First Version

---
+++ LethalCompanyTemplate/Patches.cs
@@ -0,0 +1,23 @@
+﻿using BepInEx.Logging;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace LethalPaintingSync
+{
+    [HarmonyPatch]
+    internal class Patches
+    {
+        [HarmonyBefore("LethalPaintings")]
+        [HarmonyPatch(typeof(GrabbableObject))]
+        [HarmonyPatch(nameof(GrabbableObject.SetScrapValue))]
+        static void SetScrapValuePatch(GrabbableObject __instance) 
+        {
+            // Seed the RNG with the painting's Network Object ID, thereby (hopefully) keeping the image selection synced,
+            // provided that everyone has the same set of painting textures
+            LethalPaintings.Plugin.Rand = new((int) __instance.NetworkObjectId);
+        }
+    }
+}
--- LethalCompanyTemplate/Plugin.cs
+++ LethalCompanyTemplate/Plugin.cs
@@ -1,13 +1,20 @@
 ﻿using BepInEx;
+using HarmonyLib;
+using System.Reflection;
+using UnityEngine;
 
-namespace LethalCompanyTemplate
+namespace LethalPaintingSync
 {
     [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
         {
-            // Plugin startup logic
+            // Apply another patch over LethalPaintings
+            var harmony = new Harmony("LethalPaintingSync");
+
+            harmony.PatchAll();
+
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
         }
     }
